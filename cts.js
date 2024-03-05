f94e30f670a00b3825e2163d045e15be0bd7f55d
mimi <158101519+Aalbaine@users.noreply.github.com>
2024-03-04 20:04:49 -0600

    Committing updates to project

---
+++ cts.js
@@ -0,0 +1,29 @@
+"id": 1,
+  "name": "cheri",
+  "growth_time": 3,
+  "max_harvest": 5,
+  "natural_gift_power": 60,
+  "size": 20,
+  "smoothness": 25,
+  "soil_dryness": 15,
+  "firmness": {
+    "name": "soft",
+  
+  },
+  "flavors": [
+    {
+      "potency": 15,
+      "flavor": { sour and sweet };
+        "name": "sour and sweet",
+        "url": "https://pokeapi.co/api/v2/berry-flavor/1/"
+      }
+    }
+  ],
+  "item": {
+    "name": "",
+    "url": "https://pokeapi.co/api/v2/item/126/"
+  },
+  "natural_gift_type": {
+    "name": "fire",
+    
+  }
